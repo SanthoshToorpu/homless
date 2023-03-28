747ce106ae910fccb0d4f49255fedcd8e1505258
Toorpu Santhosh <toorpusanthosh@gmail.com>
2023-03-28 02:06:57 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
-import React from 'react'
-import {Routes , Route,  Link} from "react-router-dom";
-import {Layout, Typography, Space} from 'antd';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Header, Homepage, News, Donatenow, Shelters } from './components';
-import Row from 'react-bootstrap/esm/Row';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Header,
+  Homepage,
+  News,
+  Donatenow,
+  Shelters,
+  Footer,
+} from "./components";
+import Row from "react-bootstrap/esm/Row";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const App = () => {
   return (
-    <div className='app'>
-        <div className='navbar'>
-            <Header />
-        </div>
-        <div className='main'>
-            <Layout>
-                    <div className='routes'>
-                        <Routes>
-                        <Route path='/' element={<Homepage/>} />
-                        <Route path='/Healthcare' element={<News/>} />
-                        <Route path='/Donatenow' element={<Donatenow/>} />
-                        <Route path='/Shelters' element={<Shelters/>} />
-                        
-                        </Routes>
-                    </div>
-                </Layout>
-        </div>
-        <div className='footer'>
-          <h1 className='text-center text-decoration-underline m-5'>SIGN UP FOR EMAIL UPDATES</h1>
-          
-            <Row className="m-5">
-            <Form>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form>
+    <div className="app">
+      <div className="navbar">
+        <Header />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/Healthcare" element={<News />} />
+              <Route path="/Donatenow" element={<Donatenow />} />
+              <Route path="/Shelters" element={<Shelters />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <h1 className="text-center text-decoration-underline m-5">
+        SIGN UP FOR EMAIL UPDATES
+      </h1>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-            </Row>
-            
+      <Row className="m-5">
+        <Form>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Row>
+      <div className="footer">
+        <Footer />
+
         {/* <Typography.Title level={5} style={{ color: 'black', textAlign: 'center' }}>Copyright © 2021 
           <Link to="/">
              Cryptoverse Inc.
@@ -59,9 +68,9 @@ const App = () => {
           <Link to="/exchanges">Exchanges</Link>
           <Link to="/news">News</Link>
         </Space> */}
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export {default as Header } from "./Header.jsx"
-export {default as Homepage } from "./Homepage.jsx"
-export {default as News } from "./News.jsx"
-export {default as Donatenow } from "./Donatenow.jsx"
-export {default as Shelters } from "./Shelters.jsx"
+export { default as Header } from "./Header.jsx";
+export { default as Homepage } from "./Homepage.jsx";
+export { default as News } from "./News.jsx";
+export { default as Donatenow } from "./Donatenow.jsx";
+export { default as Shelters } from "./Shelters.jsx";
+export { default as Footer } from "./Footer.jsx";
