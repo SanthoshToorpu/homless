c74193dada2eb14cb2f10c69cdcc9fa48b36b470
Toorpu Santhosh <toorpusanthosh@gmail.com>
2023-03-28 04:57:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,14 +37,14 @@ const App = () => {
       </h1>
 
       <Row className="m-5">
-        <Form>
+        <Form className="w-25">
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3 " controlId="formBasicPassword">
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
